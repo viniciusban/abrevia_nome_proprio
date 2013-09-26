b16f7cd53736820c8d658c73bd12061407c008b4
Vinicius Assef <viniciusban@gmail.com>
2013-09-26 13:08:52 -0300

    Extrai _abrevia_partes()

--- abrevia.py
+++ abrevia.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
 
 
-def abrevia_geral(nome):
-    def _abrevia(a):
-        return ["%s." % s[0] for s in a]
+def _abrevia_partes(a):
+    return ["%s." % s[0] for s in a]
 
+def abrevia_geral(nome):
     nomes = []
     partes = nome.split()
     if len(partes) == 1:
         # nao abrevia se tem um nome soh.
         return partes[0]
 
-    partes_abreviadas = _abrevia(partes[0:-1])
+    partes_abreviadas = _abrevia_partes(partes[0:-1])
     return "%s, %s" % (partes[-1],
             ' '.join(partes_abreviadas))
 
 def abrevia_descendente(nome):
-    def _abrevia(a):
-        return ["%s." % s[0] for s in a]
-
     partes = nome.split()
     if len(partes) == 1:
         # nao abrevia se tem um nome soh.
@@ -37,7 +34,7 @@ def abrevia_descendente(nome):
         i_final_para_abreviar = -1
         i_inicial_para_nao_abreviar = -1
 
-    partes_abreviadas = _abrevia(partes[0:i_final_para_abreviar])
+    partes_abreviadas = _abrevia_partes(partes[0:i_final_para_abreviar])
     return "%s, %s" % (
             ' '.join(partes[i_inicial_para_nao_abreviar:]),
             ' '.join(partes_abreviadas))

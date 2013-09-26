7b527823825d33fa937fade30c1880710d5f7c6e
Vinicius Assef <viniciusban@gmail.com>
2013-09-26 13:11:50 -0300

    Tira vars nao usadas

--- abrevia.py
+++ abrevia.py
@@ -4,24 +4,23 @@
 def _abrevia_partes(a):
     return ["%s." % s[0] for s in a]
 
+
 def abrevia_geral(nome):
-    nomes = []
     partes = nome.split()
     if len(partes) == 1:
         # nao abrevia se tem um nome soh.
         return partes[0]
 
-    partes_abreviadas = _abrevia_partes(partes[0:-1])
+    partes_abreviadas = _abrevia_partes(partes[:-1])
     return "%s, %s" % (partes[-1],
-            ' '.join(partes_abreviadas))
+                       ' '.join(partes_abreviadas))
+
 
 def abrevia_descendente(nome):
     partes = nome.split()
     if len(partes) == 1:
         # nao abrevia se tem um nome soh.
         return partes[0]
-
-    nomes = []
 
     if not partes[-1] in "filha filho junior neta neto":
         return None
@@ -36,5 +35,5 @@ def abrevia_descendente(nome):
 
     partes_abreviadas = _abrevia_partes(partes[0:i_final_para_abreviar])
     return "%s, %s" % (
-            ' '.join(partes[i_inicial_para_nao_abreviar:]),
-            ' '.join(partes_abreviadas))
+        ' '.join(partes[i_inicial_para_nao_abreviar:]),
+        ' '.join(partes_abreviadas))
